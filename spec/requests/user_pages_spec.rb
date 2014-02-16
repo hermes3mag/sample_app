d3fffb6d1aae4c84daee1a282942635cd6e4cc3e
Anthony Ortiz <ortiz.anthony@gmail.com>
2014-02-16 10:00:42 -0500

    7.6 Exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -49,6 +49,15 @@ require 'spec_helper'
         it "should create a user" do
           expect { click_button submit }.to change(User, :count)
         end
+
+        describe "after saving the user" do
+         before { click_button submit }
+         let(:user) { User.find_by(email: 'user@example.com') }
+
+         it { should have_title(user.name) }
+         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        end
+
       end
 
     end
